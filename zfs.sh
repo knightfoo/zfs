1e31f77f1d57e12ae755d0d1fa9bbfd1b12635b7
root <admin@admin.pl>
2017-10-18 14:22:08 +0000

    zfs.sh

--- zfs.sh
+++ zfs.sh
@@ -2,6 +2,7 @@
 
 #https://github.com/zfsonlinux/zfs/wiki/Ubuntu-16.04-Root-on-ZFS
 
+zpool_name=tank0
 
 DISKS_byid=()
 DISKS_dev=()
@@ -16,15 +17,6 @@ disks() {
 }
 
 clean_disks() {
-	lsblk -io KNAME,TYPE| grep disk | awk '{print $1}' | while read d_; 
-	do
-		echo "Dysk $d_"
-		sgdisk --zap-all /dev/${d_}
-	done
-
-}
-
-clean_diskss() {
 	for d_ in ${DISKS_dev[@]};
 	do
 		echo "Czyszcze dysk $d_"
@@ -34,11 +26,19 @@ clean_diskss() {
 
 
 partition_disks() {
-	exit
+	for d_ in ${DISKS_byid[@]};
+	do 
+		sgdisk -a1 -n2:34:2047  -t2:EF02 /dev/disk/by-id/${d_} 
+	done
+}	
+
+create_zpool() {
+	
+
 }
 
 
 disks
 echo "Dyski - ${DISKS_dev[@]} "
 
-clean_diskss
+create_zpool

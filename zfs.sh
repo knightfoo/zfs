0f71cddf8467af1b1b469fdfa8083bb9237b5a02
root <admin@admin.pl>
2017-10-19 15:26:43 +0000

    zfs 0 create-zpool

--- zfs.sh
+++ zfs.sh
@@ -12,8 +12,8 @@ disks() {
 	while read d_; 
 	do 
 		d_id=$(ls -la /dev/disk/by-id/ |grep ${d_}$ | awk '{print $9}')
-		DISKS_byid+=("${d_id}")
-		DISKS_dev+=("${d_}")
+		DISKS_byid+=("/dev/disk/by-id/${d_id}")
+		DISKS_dev+=("/dev/${d_}")
 	done< <(lsblk -io KNAME,TYPE| grep disk | awk '{print $1}')
 }
 
@@ -21,7 +21,7 @@ clean_disks() {
 	for d_ in ${DISKS_dev[@]};
 	do
 		echo "Czyszcze dysk $d_"
-		sgdisk --zap-all /dev/${d_}
+		sgdisk --zap-all ${d_}
 	done
 }
 
@@ -29,11 +29,11 @@ clean_disks() {
 partition_disks() {
 	for d_ in ${DISKS_byid[@]};
 	do 
-		sgdisk -a1 -n2:34:2047  -t2:EF02 /dev/disk/by-id/${d_} 
+		sgdisk -a1 -n2:34:2047  -t2:EF02 ${d_} 
 	done
 }	
 
-create_zpool() {
+create_zpool_() {
 	ile=0
 	dyski=""
 	#zpool create -o ashift=12 -O atime=off -O canmount=off -O compression=lz4 -O normalization=formD -O mountpoint=/ -R /mnt tank0 mirror /dev/disk/by-id/ata-VBOX_HARDDISK_VB679d4313-9a878091 /dev/disk/by-id/ata-VBOX_HARDDISK_VB9c66a80e-e138249a mirror /dev/disk/by-id/ata-VBOX_HARDDISK_VBb2041ff0-9c27fdcc /dev/disk/by-id/ata-VBOX_HARDDISK_VBbd4505a1-546c1f04 	
@@ -43,20 +43,22 @@ create_zpool() {
 		if [ $ile -lt 2 ];
 		then
 			ile=$((${ile}+1))
-			dyski="$dyski /dev/$d_"
+			dyski="$dyski $d_"
+			echo "Create"
 		else
 			span=$((${span}+1))
-			ile=1
+			#ile=1
 			dyski=""
-			dyski="$dyski /dev/$d_"
+			dyski="$dyski $d_"
+			echo "append"
 		fi
 		
-		[ ${ile} -eq 2 ] && (echo "Creating: $dyski") 
+		#[ ${ile} -eq 2 ] && (echo "Creating: $dyski") 
 	done
 
 }
 
-create_zpool_() {
+create_zpool() {
 	SPAN=0
 	SPANS=$((${#DISKS_byid[@]}/2))
 	dyski=""
@@ -71,9 +73,10 @@ create_zpool_() {
 	do
  		if [ ${SPAN} -eq 0 ]
  		then
-  			echo "zpool create ${POOL} mirror `echo | awk -v span=${SPAN} -v zfsparts="${DISKS_byid[@]}" '{ split(zfsparts,arr," "); print arr[span+span+1] " " arr[span+span+2] }'`"
+  			zpool create ${zpool_name} mirror `echo | awk -v span=${SPAN} -v zfsparts="${DISKS_byid[@]}" '{ split(zfsparts,arr," "); print arr[span+span+1] " " arr[span+span+2] }'`
  		else
-  			echo "zpool add ${POOL} mirror `echo | awk -v span=${SPAN} -v zfsparts="${DISKS_byid[@]}" '{ split(zfsparts,arr," "); print arr[span+span+1] " " arr[span+span+2] }'`"
+  			#zpool add ${zpool_name} mirror `echo | awk -v span=${SPAN} -v zfsparts="${DISKS_byid[@]}" '{ split(zfsparts,arr," "); print arr[span+span+1] " " arr[span+span+2] }'`
+			exit
  		fi
         	SPAN=$((${SPAN}+1))
 	done
@@ -85,4 +88,4 @@ create_zpool_() {
 
 disks
 
-create_zpool_
+create_zpool
